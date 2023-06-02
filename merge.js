eaadb285c7c7d5485d52da113961ca03dc36b7be
William J Squires <william.squiresii@gmail.com>
2023-06-02 14:26:00 -0500

    add mergeSort iteratively

--- merge.js
+++ merge.js
@@ -24,20 +24,48 @@ function merge(arr1, arr2) {
   return out;
 }
 
-function mergeSort(arr) {
-  // base case = array with 1 or 0 elements inside
-  if (arr.length <= 1) {
-    return arr;
-  }
-  // otherwise, split array in half over and over.
-  const arr1 = arr.slice(0, arr.length / 2);
-  const arr2 = arr.slice((arr.length / 2), arr.length);
-  console.log('arr1=', arr1, 'arr2=', arr2)
-  const sortedArr1 = mergeSort(arr1);
-  const sortedArr2 = mergeSort(arr2);
+// function mergeSort(arr) {
+//   // base case = array with 1 or 0 elements inside
+//   if (arr.length <= 1) {
+//     return arr;
+//   }
+//   // otherwise, split array in half over and over.
+//   const arr1 = arr.slice(0, arr.length / 2);
+//   const arr2 = arr.slice((arr.length / 2), arr.length);
+//   console.log('arr1=', arr1, 'arr2=', arr2)
+//   const sortedArr1 = mergeSort(arr1);
+//   const sortedArr2 = mergeSort(arr2);
 
-  console.log('our return statement', merge(sortedArr1, sortedArr2))
-  return merge(sortedArr1, sortedArr2);
+//   console.log('our return statement', merge(sortedArr1, sortedArr2))
+//   return merge(sortedArr1, sortedArr2);
+// }
+
+function mergeSort(arr) {
+  const queue = [arr.slice(0, arr.length)]
+
+  function _checkIfAllShort(queue) {
+    for (let arr of queue) {
+      if (arr.length > 1) {
+        return false
+      }
+    }
+    return true
+  }
+
+  while(!_checkIfAllShort(queue)) {
+    const curr = queue.shift()
+    queue.push(curr.slice(0, curr.length / 2))
+    queue.push(curr.slice((curr.length / 2), curr.length))
+  }
+
+  while (queue.length > 1) {
+    const arr1 = queue.shift()
+    const arr2 = queue.shift()
+    const merged = merge(arr1, arr2)
+    queue.push(merged)
+  }
+
+  return(queue[0]);
 }
 
 module.exports = { merge, mergeSort};
