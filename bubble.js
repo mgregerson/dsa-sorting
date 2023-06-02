938151c8c569772a873f1c56a43159f34ebb201b
mgregerson <mgregerson4321@gmail.com>
2023-06-02 14:29:00 -0400

    Completed bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,16 @@
-function bubbleSort() {}
+function bubbleSort(nums) {
+  // loop over nums arr
+  for (let i = 0; i < nums.length; i++){
+    // loop over nums arr -1
+    for (let j = 0; j < nums.length - i; j++){
+      if (nums[j] > nums[j+1]) {
+        let temp = nums[j];
+        nums[j] = nums[j+1];
+        nums[j+1] = temp;
+      }
+    }
+  }
+  return nums;
+}
 
 module.exports = bubbleSort;
